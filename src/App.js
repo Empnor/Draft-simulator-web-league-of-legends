508353dcba653706aa05837f48f055e82efa6419
Empnor <eemeli.noronen@gmail.com>
2024-06-09 11:28:28 +0300

    champion picking
    you can pick champions now for red and blue team

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import placeholder from './placeholder.png';
 
 function App() {
   const [champions, setChampions] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [blueteam, setblueteam] = useState(new Array(5).fill(placeholder));
+  const [Redteam, setRedteam] = useState(new Array(5).fill(placeholder));
+  const [selectedPlaceholder, setSelectedPlaceholder] = useState(null);
 
   useEffect(() => {
     fetch('champions.json')
@@ -16,27 +20,75 @@ function App() {
     setSearchTerm(event.target.value);
   };
 
+  const handlePlaceholderClick = (column, index) => {
+    setSelectedPlaceholder({ column, index });
+  };
+
+  const handleChampionClick = champion => {
+    if (selectedPlaceholder) {
+      const { column, index } = selectedPlaceholder;
+      if (column === 'left') {
+        const updatedblueteam = [...blueteam];
+        updatedblueteam[index] = champion;
+        setblueteam(updatedblueteam);
+      } else if (column === 'right') {
+        const updatedRedteam = [...Redteam];
+        updatedRedteam[index] = champion;
+        setRedteam(updatedRedteam);
+      }
+      setSelectedPlaceholder(null);
+    }
+  };
+
   const filteredChampions = champions.filter(champion =>
     champion.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
     <div className="App">
-      <h1>Draft Simulator</h1>
-      <div className="search-container">
+      <h1>Champion List</h1>
+      <div className="SearchContainer">
         <input
           type="text"
-          placeholder="Search for a champion"
+          placeholder="Draft Simulator"
           value={searchTerm}
           onChange={handleChange}
         />
       </div>
-      <div className="champion-container">
-        <div className="champion-list">
+      <div className="ChampionContainer">
+        <div className="column">
+          <h2 className='Red'>Red team</h2>
+          {blueteam.map((champion, index) => ( 
+            <div 
+              key={index} 
+              className={`ChampionPlaceholder ${selectedPlaceholder && selectedPlaceholder.column === 'left' && selectedPlaceholder.index === index ? 'selected' : ''}`}
+              onClick={() => handlePlaceholderClick('left', index)}
+            >
+              <img src={champion === placeholder ? placeholder : `champion/${champion}.png`} alt={champion} />
+            </div>
+          ))}
+        </div>
+        <div className="ChampionList">
           {filteredChampions.map((champion, index) => (
-            <div key={index} className="champion">
+            <div
+              key={index}
+              className="champion"
+              onClick={() => handleChampionClick(champion)}
+            >
               <img src={`champion/${champion}.png`} alt={champion} />
-              <div className="champion-name">{champion}</div>
+              <div className="ChampionName">{champion}</div>
+            </div>
+          ))}
+        </div>
+        <div className="column">
+          <h2 className='Blue'>Blue team</h2>
+          {Redteam.map((champion, index) => (
+            <div 
+              key={index} 
+              className={`ChampionPlaceholder ${selectedPlaceholder && selectedPlaceholder.column === 'right' && selectedPlaceholder.index === index ? 'selected' : ''}`}
+              onClick={() => handlePlaceholderClick('right', index)}
+            >
+              <img src={champion === placeholder ? placeholder : `champion/${champion}.png`} alt={champion} />
             </div>
           ))}
         </div>
